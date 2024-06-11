37149d034e4eac7ffbf1a6051660cdf6a6b28c8a
Skyler <skyler881206@gmail.com>
2024-06-11 16:16:07 +0800

    Update the hdr merge function

--- utils.py
+++ utils.py
@@ -1,6 +1,11 @@
 import torch
+import cv2
 
 def transfer_to_cuda_and_float32(tensor_dict):
     for key in tensor_dict:
         tensor_dict[key] = tensor_dict[key].to(torch.device('cuda')).float()
     return tensor_dict
+
+def merge_hdr(images: dict):
+    merge_debevec = cv2.createMergeDebevec()
+    return merge_debevec.process(images['images'], images['times'])
